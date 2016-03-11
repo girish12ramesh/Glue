a2c6bd6c3e06505dadc91f9ad8837440fdf6654f
Rohit <rohitshampur09@yahoo.com>
2016-03-11 06:55:45 +0530

    update build.gradle and testglue example app

--- testglue/src/main/java/com/rohitshampur/testglue/MainActivity.java
+++ testglue/src/main/java/com/rohitshampur/testglue/MainActivity.java
@@ -3,6 +3,8 @@ package com.rohitshampur.testglue;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     @StickToView
     private TextView textView;
 
+    @StickToView(R.id.button)
+    private Button button;
+
     @StickToResource(R.string.app_name)
     private String name;
 
@@ -25,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
         Glue.stickTo(this);
         textView.setText("Testing glue");
         Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
-        startActivity(new Intent(this, FragmentActivity.class));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FragmentActivity.class));
+            }
+        });
+
     }
 }
